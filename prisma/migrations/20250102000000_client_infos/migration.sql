d8111f9db7f7f348d0f30a10843474c01d03119e
khouloud bhar <bharkhouloud16@gmail.com>
2026-09-03 13:43:19 +0200

    Create migration.sql

---
+++ prisma/migrations/20250102000000_client_infos/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Client" ADD COLUMN "telephone" TEXT;
+ALTER TABLE "Client" ADD COLUMN "identifiantEntreprise" TEXT;
+ALTER TABLE "Client" ADD COLUMN "formeJuridique" TEXT;
